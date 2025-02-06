b8890469273c4b0d010717d0e792dc6751014f74
viniciuslemes10 <vinikjhgfds@gmail.com>
2025-02-06 15:02:15 -0300

    feat: add summary field

--- src/main/java/com/vinidev/btgpactual/controller/dto/ApiResponse.java
+++ src/main/java/com/vinidev/btgpactual/controller/dto/ApiResponse.java
@@ -2,9 +2,8 @@ package com.vinidev.btgpactual.controller.dto;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
-public record ApiResponse<T>(Map<String, Objects> summary,
+public record ApiResponse<T>(Map<String, Object> summary,
                              List<T> data,
                              PaginationResponse pagination) {
 }
