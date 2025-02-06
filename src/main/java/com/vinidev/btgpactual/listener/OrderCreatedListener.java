0b3b8e02a2501f88c3bb730a00f3d01f77f27757
viniciuslemes10 <vinikjhgfds@gmail.com>
2025-02-06 12:13:25 -0300

    feat: logic to consume the request from the queue

--- src/main/java/com/vinidev/btgpactual/listener/OrderCreatedListener.java
+++ src/main/java/com/vinidev/btgpactual/listener/OrderCreatedListener.java
@@ -2,6 +2,7 @@ package com.vinidev.btgpactual.listener;
 
 import com.vinidev.btgpactual.config.RabbitMQConfig;
 import com.vinidev.btgpactual.listener.dto.OrderCreatedEvent;
+import com.vinidev.btgpactual.service.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -13,9 +14,17 @@ public class OrderCreatedListener {
 
     private final Logger logger = LoggerFactory.getLogger(OrderCreatedListener.class);
 
+    private final OrderService orderService;
+
+    public OrderCreatedListener(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
     @RabbitListener(queues = RabbitMQConfig.ORDER_CREATE_QUEUE)
     public void listener(Message<OrderCreatedEvent> message) {
         logger.info("Starting queue consumption in RabbitMQ!");
         logger.info("Message consumed: {}", message);
+
+        orderService.save(message.getPayload());
     }
 }
