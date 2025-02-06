8ea667a3c581c12dea9ebc7683a1bca2a2b04b7c
viniciuslemes10 <vinikjhgfds@gmail.com>
2025-02-06 14:44:42 -0300

    feat: searching for all orders related to customer id

--- src/main/java/com/vinidev/btgpactual/repository/OrderRepository.java
+++ src/main/java/com/vinidev/btgpactual/repository/OrderRepository.java
@@ -1,7 +1,10 @@
 package com.vinidev.btgpactual.repository;
 
 import com.vinidev.btgpactual.entity.OrderEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface OrderRepository extends MongoRepository<OrderEntity, Long> {
+    Page<OrderEntity> findAllByCustomerId(Long costumerId, PageRequest pageRequest);
 }
