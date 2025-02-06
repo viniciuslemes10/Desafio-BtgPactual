e4682ac4ce55870e8df0e327647fe6c73fa0f305
viniciuslemes10 <vinikjhgfds@gmail.com>
2025-02-06 14:57:57 -0300

    feat: add query to calculate the total value of products per customer

--- src/main/java/com/vinidev/btgpactual/service/OrderService.java
+++ src/main/java/com/vinidev/btgpactual/service/OrderService.java
@@ -5,19 +5,27 @@ import com.vinidev.btgpactual.entity.OrderEntity;
 import com.vinidev.btgpactual.entity.OrderItem;
 import com.vinidev.btgpactual.listener.dto.OrderCreatedEvent;
 import com.vinidev.btgpactual.repository.OrderRepository;
+import org.bson.Document;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
+
 @Service
 public class OrderService {
-    private final OrderRepository orderRepository;
 
-    public OrderService(OrderRepository orderRepository) {
+    private final OrderRepository orderRepository;
+    private final MongoTemplate mongoTemplate;
+
+    public OrderService(OrderRepository orderRepository, MongoTemplate mongoTemplate) {
         this.orderRepository = orderRepository;
+        this.mongoTemplate = mongoTemplate;
     }
 
     public void save(OrderCreatedEvent event) {
@@ -35,6 +43,17 @@ public class OrderService {
         var orders = orderRepository.findAllByCustomerId(costumerId, pageRequest);
 
         return orders.map(OrderResponse::fromEntity);
+    }
+
+    public BigDecimal findTotalOnOrdersByCustomerId(Long customerId) {
+        var aggregations = newAggregation(
+                match(Criteria.where("customerId").is(customerId)),
+                group().sum("total").as("total")
+        );
+
+        var response = mongoTemplate.aggregate(aggregations, "tb_orders", Document.class);
+
+        return (BigDecimal) response.getUniqueMappedResult().getOrDefault("total", BigDecimal.ZERO);
     }
 
     private BigDecimal getTotal(OrderCreatedEvent event) {
