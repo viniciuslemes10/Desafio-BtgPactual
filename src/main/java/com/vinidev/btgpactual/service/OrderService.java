825d48752a07e434b793fb32109799dd779679ce
viniciuslemes10 <vinikjhgfds@gmail.com>
2025-02-06 12:12:40 -0300

    feat: creating the service and persisting in the database

---
+++ src/main/java/com/vinidev/btgpactual/service/OrderService.java
@@ -0,0 +1,43 @@
+package com.vinidev.btgpactual.service;
+
+import com.vinidev.btgpactual.entity.OrderEntity;
+import com.vinidev.btgpactual.entity.OrderItem;
+import com.vinidev.btgpactual.listener.dto.OrderCreatedEvent;
+import com.vinidev.btgpactual.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public void save(OrderCreatedEvent event) {
+        var entity = new OrderEntity();
+
+        entity.setOrderId(event.codigoPedido());
+        entity.setCustomerId(event.codigoCliente());
+        entity.setItems(getOrderItems(event));
+        entity.setTotal(getTotal(event));
+
+        orderRepository.save(entity);
+    }
+
+    private BigDecimal getTotal(OrderCreatedEvent event) {
+        return event.itens().stream()
+                .map(i -> i.preco().multiply(BigDecimal.valueOf(i.quantidade())))
+                .reduce(BigDecimal::add)
+                .orElse(BigDecimal.ZERO);
+    }
+
+    private static List<OrderItem> getOrderItems(OrderCreatedEvent event) {
+        return event.itens().stream()
+                .map(i -> new OrderItem(i.produto(), i.quantidade(), i.preco()))
+                .toList();
+    }
+}
