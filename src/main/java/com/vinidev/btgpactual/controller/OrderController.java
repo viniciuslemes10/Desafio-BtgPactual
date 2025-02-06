6f26f973575c0cc55791184eb9b76822ec08b246
viniciuslemes10 <vinikjhgfds@gmail.com>
2025-02-06 14:45:12 -0300

    feat: searching for all orders related to customer id

---
+++ src/main/java/com/vinidev/btgpactual/controller/OrderController.java
@@ -0,0 +1,34 @@
+package com.vinidev.btgpactual.controller;
+
+import com.vinidev.btgpactual.controller.dto.ApiResponse;
+import com.vinidev.btgpactual.controller.dto.OrderResponse;
+import com.vinidev.btgpactual.controller.dto.PaginationResponse;
+import com.vinidev.btgpactual.service.OrderService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/customers/{customerId}/orders")
+    public ResponseEntity<ApiResponse<OrderResponse>> listOrders(
+            @RequestParam(name = "page", defaultValue = "0") Integer page,
+            @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize,
+            @PathVariable("customerId") Long costumerId) {
+
+        var pageResponse = orderService.findAllCustomerId(costumerId, PageRequest.of(page, pageSize));
+
+        return ResponseEntity.ok(new ApiResponse<>(
+                pageResponse.getContent(),
+                PaginationResponse.fromPage(pageResponse)
+        ));
+    }
+}
